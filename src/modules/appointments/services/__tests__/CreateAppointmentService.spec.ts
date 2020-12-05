c8f7a17c71db8b0ed32f196699ecda35523c46ac
Mário Fernandes <mariobmf02@gmail.com>
2020-12-05 17:52:41 -0300

    feat: unit tests refactored

--- src/modules/appointments/services/__tests__/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/__tests__/CreateAppointmentService.spec.ts
@@ -3,13 +3,15 @@ import AppError from '@shared/errors/AppError';
 import FakeAppointmentsRepository from '@modules/appointments/repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from '../CreateAppointmentService';
 
-describe('CreateAppointment', () => {
-  it('should be able to create a new appointment', async () => {
-    const fakeAppointmentRepository = new FakeAppointmentsRepository();
-    const createAppointment = new CreateAppointmentService(
-      fakeAppointmentRepository,
-    );
+let fakeAppointmentRepository: FakeAppointmentsRepository;
+let createAppointment: CreateAppointmentService;
 
+describe('CreateAppointment', () => {
+  beforeEach(() => {
+    fakeAppointmentRepository = new FakeAppointmentsRepository();
+    createAppointment = new CreateAppointmentService(fakeAppointmentRepository);
+  });
+  it('should be able to create a new appointment', async () => {
     const appointment = await createAppointment.execute({
       date: new Date(),
       providerId: '123456',
@@ -20,17 +22,12 @@ describe('CreateAppointment', () => {
   });
 
   it('should not be able to create two appointments on the same time', async () => {
-    const fakeAppointmentRepository = new FakeAppointmentsRepository();
-    const createAppointment = new CreateAppointmentService(
-      fakeAppointmentRepository,
-    );
-
     await createAppointment.execute({
       date: new Date(),
       providerId: '123456',
     });
 
-    expect(
+    await expect(
       createAppointment.execute({
         date: new Date(),
         providerId: '123456',
