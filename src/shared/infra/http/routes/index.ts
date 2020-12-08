6542b8a4639b48b17ff1fff35eacba013c0cf542
Mário Fernandes <mariobmf02@gmail.com>
2020-12-08 16:43:39 -0300

    feat: adicionado recurso para listar todos os provedores

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import usersRouter from '@modules/users/infra/http/routes/users.routes';
 import appointmentsRouter from '@modules/appointments/infra/http/routes/appointments.routes';
+import providersRouter from '@modules/appointments/infra/http/routes/providers.routes';
 import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
 import passwordRouter from '@modules/users/infra/http/routes/password.routes';
 import profileController from '@modules/users/infra/http/routes/profile.routes';
@@ -10,6 +11,7 @@ const routes = Router();
 
 routes.use('/users', usersRouter);
 routes.use('/appointments', appointmentsRouter);
+routes.use('/providers', providersRouter);
 routes.use('/sessions', sessionsRouter);
 routes.use('/password', passwordRouter);
 routes.use('/profile', profileController);
